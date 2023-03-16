5f1bfb3c0c9ce7617f400f2cf3f3f5a22bc90233
Aravind <ajayaravind797@gmail.com>
2023-03-15 17:46:54 -0700

    Edit Method done by Aravinth.

--- routes/employeeRouter.js
+++ routes/employeeRouter.js
@@ -63,23 +63,92 @@ employeeRouter.post("/save", (req, res, next) => {
       photo: fileDoc,
     });
     await employee.save();
+    if (employee) {
+      res.send(employee);
+    } else {
+      res.status(404).send({ message: "Women Product Not Found" });
+    }
   });
 });
 
-employeeRouter.get('/get', expressAsyncHandler(async(req, res) => {
+employeeRouter.put(
+  "/update/:id",
+  expressAsyncHandler(async (req, res) => {
+
+    const employeeId = req.params.id;
+
+    const employeeUpdate = await Employee.findById(employeeId);
+
+    fileDocPath = "";
+
+    var upload = multer({ storage: storage }).fields([
+      { name: "file", maxCount: 1 },
+    ]);
+    upload(req, res, async function (err) {
+
+      console.log("req", req.body);
+      console.log("req", req.files);
+
+      let fileDoc = fileDocPath;
+
+      if (employeeUpdate.photo === req.body.file) {
+        fileDoc = req.body.file;
+      } else {
+        fileDoc = fileDocPath;
+      }
+
+      req.body.photo = fileDoc;
+      if (employeeUpdate) {
+        employeeUpdate.fname = req.body.fname;
+        employeeUpdate.lname = req.body.lname;
+        employeeUpdate.mnumber = req.body.mnumber;
+        employeeUpdate.email = req.body.email;
+        employeeUpdate.address = req.body.address;
+        employeeUpdate.address2 = req.body.address2;
+        employeeUpdate.city = req.body.city;
+        employeeUpdate.state = req.body.state;
+        employeeUpdate.pincode = req.body.pincode;
+        employeeUpdate.cname = req.body.cname;
+        employeeUpdate.photo = fileDoc;
+
+        const UdatedEmployee = await employeeUpdate.save();
+        res.send({ message: " Updated", employeeUpdate: UdatedEmployee });
+      } else {
+        res.status(404).send({ message: "Employee Update Not Found" });
+      }
+    });
+  })
+);
+
+employeeRouter.get(
+  "/get",
+  expressAsyncHandler(async (req, res) => {
     const app = await Employee.find();
 
     if (app) {
-        res.send(app);
+      res.send(app);
     } else {
-        res.status(404).send({ message: 'Women Product Not Found' });
+      res.status(404).send({ message: "Women Product Not Found" });
     }
-}));
+  })
+);
 
+employeeRouter.get(
+  "/get/:id",
+  expressAsyncHandler(async (req, res) => {
+    const app = await Employee.findById(req.params.id);
+    if (app) {
+      res.send(app);
+    } else {
+      res.status(404).send({ message: "Women Product Not Found" });
+    }
+  })
+);
 
-employeeRouter.delete("/delete/:id",expressAsyncHandler(async (req, res) => {
-
-  console.log("req", req.params.id);
+employeeRouter.delete(
+  "/delete/:id",
+  expressAsyncHandler(async (req, res) => {
+    console.log("req", req.params.id);
 
     const deleteApp = await Employee.findById(req.params.id);
     if (deleteApp) {
